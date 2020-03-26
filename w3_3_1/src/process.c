7b079aed94c5fb377833fadba0b657d0415794d8
Vagacoder <qiruihu@gmail.com>
2020-03-26 01:16:32 -0700

    init repo

---
+++ w3_3_1/src/process.c
@@ -0,0 +1,39 @@
+/*
+ ============================================================================
+ Name        : process.c
+ Author      : Vagacoder
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Study Unix/Linux System Call fork(),
+ 	 	 	 	 which creates a new process from parent process,
+ 	 	 	 	 in parent process, the return value is pid of child process;
+ 	 	 	 	 in child process, the return value is 0.
+
+ 	 	 	 	 Parent waits for child complete;
+ ============================================================================
+ */
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	pid_t pid;
+	pid = fork();
+	if(pid == 0){
+		printf("parent is waiting for child...\n");
+		wait(NULL);
+		printf("child complete!");
+	} else if (pid > 0){
+		printf("child is working!\n");
+		execlp("bin/ls", "ls", NULL);
+		printf("child is done!\n");
+	} else{
+		printf("fork error!\n");
+	}
+
+	return EXIT_SUCCESS;
+}
